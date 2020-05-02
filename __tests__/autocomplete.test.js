233182affc3143f8ec970ee2b7d0e79cd623c7cf
Musa Abdillah <musa@clappingape.com>
2020-05-02 11:05:02 +0700

    autocomplete project with react js, express & mongodb

---
+++ __tests__/autocomplete.test.js
@@ -0,0 +1,51 @@
+const rooms = [
+	{"_id": "5622eb1f105807ceb6ad868b", "name": "react"},
+	{"_id": "5622eb1f105807ceb6ad868c", "name": "node"},
+	{"_id": "5622eb1f105807ceb6ad868d", "name": "angular"},
+	{"_id": "5622eb1f105807ceb6ad868e", "name": "backbone"},
+	{"_id": "5622eb1f105807ceb6ad868f", "name": "express"},
+	{"_id": "5622eb1f105807ceb6ad868g", "name": "rails"}
+]
+
+const 	TestUtils = require("react-addons-test-utils"),
+		React = require("react"),
+		ReactDOM = require("react-dom"),
+		Autocomplete = require("../src/autocomplete.jsx"),
+		fD = ReactDOM.findDOMNode
+const autocomplete = TestUtils.renderIntoDocument(
+		React.createElement(Autocomplete, {
+			options: rooms,
+			url: "test"
+		})
+	)
+
+const optionName = TestUtils.findRenderedDOMComponentWithClass(autocomplete, "options-name")
+
+describe("Autocomplete", () => {
+	it("have four initial options", () => {
+		var options = TestUtils.scryRenderedDOMComponentsWithClass(
+			autocomplete,
+			"options-list-item"
+		)
+		expect(options.length).toBe(4)
+	})
+
+	it("change options based on the input", () => {
+		expect(fD(optionName).value).toBe("")
+		fD(optionName).value = "r"
+		TestUtils.Simulate.change(fD(optionName))
+		expect(fD(optionName).value).toBe("r")
+		options = TestUtils.scryRenderedDOMComponentsWithClass(autocomplete, "options-list-item")
+		expect(options.length).toBe(1)
+		expect(fD(options[0]).textContent).toBe("#react")
+	})
+
+	it("offer to save option when there are no matches", () => {
+		fD(optionName).value = "ember"
+TestUtils.Simulate.change(fD(optionName))
+		options = TestUtils.scryRenderedDOMComponentsWithClass(autocomplete, "options-list-item")
+		expect(options.length).toBe(0)
+		var optionAdd = TestUtils.findRenderedDOMComponentWithClass(autocomplete, "options-add")
+		expect(fD(optionAdd).textContent).toBe("Add #ember")
+	})
+})
---
+++ src/autocomplete.jsx
@@ -0,0 +1,110 @@
+const React = require("react")
+const ReactDOM = require("react-dom")
+const request = require("axios")
+const fD = ReactDOM.findDOMNode
+class Autocomplete extends React.Component {
+	constructor(props){
+		super(props)
+		this.state = {
+			options: this.props.options,
+			filteredOptions: this.props.options,
+			currentOption: ''
+		}
+		this.filter = this.filter.bind(this)
+		this.addOption = this.addOption.bind(this)
+		this.printSomething = this.printSomething.bind(this)
+	}
+
+	componentDidMount(){
+		if (this.props.url == "test") return true
+		request({url: this.props.url})
+			.then(response => response.data)
+			.then(body => {
+				if (!body) {
+					console.error("Failed to load")
+				}
+				this.setState({options: body})
+			})
+			.catch(console.error)
+	}
+
+	filter(event){
+		console.log("PRINT EVENT===================================")
+		console.log(event)
+		console.log("PRINT EVENT===================================")
+		this.setState({
+			currentOption: event.target.value,
+			filteredOptions: (this.state.options.filter(function(options, index, list) {
+				return(event.target.value === options.name.substr(0, event.target.value.length))
+			}))
+		})
+	}
+
+	addOption(event){
+		let currentOption = this.state.currentOption
+		request.post(this.props.url, {name: currentOption})
+			.then(response => response.data)
+			.then((body) => {
+				if (!body) {
+					return console.error("Failed to save")
+				}
+				this.setState(
+					{options: [body].concat(this.state.options)},
+					() => { this.filter({target: {value: currentOption}}) }
+				)
+			})
+			.catch(error => { return console.error("Failed to save")})
+	}
+
+	printSomething(e){
+		console.log("==========================")
+		console.log("PRINT SOMETHING TO ME!")
+		console.log("==========================")
+	}
+
+	render(){
+		return(
+			<div className="form-group">
+				<input type="text"
+		          onKeyUp={(event)=>(event.keyCode==13)?this.addOption():''}
+		          className="form-control option-name"
+		          onChange={this.filter}
+		          value={this.currentOption}
+		          placeholder="React.js" 
+				>
+				</input>
+				{
+					this.state.filteredOptions.map(function(option, index, list){
+						return(
+							<div key={option._id}>
+								<a className="btn btn-default option-list-item" href={"/#/" + option.name} target="_blank">
+									#{option.name}
+								</a>
+							</div>
+						)
+					})
+				}
+						{console.log("================================")}
+						{console.log("YOU SHOULD HERE! filteredOptions")}
+						{console.log(this.state.filteredOptions)}
+						{console.log("YOU SHOULD HERE! filteredOptions")}
+						{console.log("================================")}
+
+						{console.log("IS CONDITION TRUE================================")}
+						{console.log(this.state.filteredOptions.length == 0 && this.state.currentOption != "")}
+						{console.log("IS CONDITION TRUE================================")}
+				{(() => {
+					if (this.state.filteredOptions.length == 0 && this.state.currentOption != "") {
+						return(
+							<a className="btn btn-info option-add" onClick={this.addOption}>
+								Add #{this.state.currentOption}
+							</a>
+						)
+					}	
+				})()}
+			</div>
+		)
+	}
+}
+
+module.exports = Autocomplete
