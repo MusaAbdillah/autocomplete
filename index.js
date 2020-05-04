db28f70f72838742d8838f6362d76bfd37bb2db9
Musa Abdillah <musa@clappingape.com>
2020-05-04 13:55:53 +0700

    remove proxy and add corsOptions

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const 	express = require("express"),
+		cors = require('cors'),
 		app = express(),
 		mongodb = require("mongodb"),
 		bodyParser = require("body-parser"),
@@ -10,8 +11,7 @@ const 	express = require("express"),
 		url = "mongodb://heroku_qvb1q40q:pj1qaamqtr1v640aeb80lnkos2@ds245762.mlab.com:45762/heroku_qvb1q40q",
 		ReactDOM = require("react-dom"),
 		ReactDOMServer = require("react-dom/server"),
-		React = require("react"),
-		cors = require('cors')
+		React = require("react")
 
 
 require("babel-register")({
@@ -34,16 +34,22 @@ const port = process.env.PORT || 8080
 //   }
 // }
 
+
+const corsOptions = {
+	origin: '*',
+	optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
 mongodb.MongoClient.connect(url, function(err, db) {
 	if (err) {
 		console.error(err)
 		process.exit(1)
 	}
 
-	app.use(cors())
 	app.use(compression())
 	app.use(logger("div"))
 	app.use(errorHandler())
+	app.use(cors())
 	app.use(bodyParser.urlencoded({extended: true}))
 	app.use(bodyParser.json())
 	app.use(validator())
@@ -52,13 +58,11 @@ mongodb.MongoClient.connect(url, function(err, db) {
 	app.set("view engine", "handlebars")
 
 	app.use(function(req, res, next) {
-		res.header("Access-Control-Allow-Origin", "*")
-		res.header("Access-Control-Allow-Headers", "X-Requested-With")
 		req.rooms = db.collection("rooms")
 		return next()
 	})
 
-	app.get("/rooms", cors(), function(req, res, next) {
+	app.get("/rooms", cors(corsOptions), function(req, res, next) {
 		req.rooms
 			.find({}, {sort: {_id: -1}})
 			.toArray(function(err, docs) {
@@ -67,7 +71,7 @@ mongodb.MongoClient.connect(url, function(err, db) {
 			})
 	})
 
-	app.post("/rooms", cors(), function(req, res, next) {
+	app.post("/rooms", cors(corsOptions), function(req, res, next) {
 		req.checkBody("name", "Invalid name in body").notEmpty()
 		var errors = req.validationErrors()
 		if (errors) return next(errors)
@@ -77,7 +81,7 @@ mongodb.MongoClient.connect(url, function(err, db) {
 		}) 
 	})
 
-	app.get("/", cors(), function(req, res, next) {
+	app.get("/", cors(corsOptions), function(req, res, next) {
 		var url = "mongodb://heroku_qvb1q40q:pj1qaamqtr1v640aeb80lnkos2@ds245762.mlab.com:45762/heroku_qvb1q40q"
 		req.rooms
 			.find({}, {sort: {_id: -1}})
